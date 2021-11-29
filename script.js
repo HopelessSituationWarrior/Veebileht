fa83bff153a8dbf95bc5dcc36f58740443a94da4
Rasmus Peäske <92918669+RasmusPeaske@users.noreply.github.com>
2021-11-29 20:06:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* Funktsioon popupi loomiseks, kui veebilehe kasutaja klikib vastavalt "OSTA" nupule. */
+
 function togglePopup() {
     document.getElementById("popup-1").classList.toggle("active");
 }
